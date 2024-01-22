73b71e3894a1d4554e602887ed7d11c2fa97c9cc
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2024-01-22 11:19:55 +0100

    utility folder for spacing, font size, color

---
+++ src/utils/fontsize.jsx
@@ -0,0 +1,9 @@
+export const fontsizes = {
+    xs: '5px',
+    sm: '10px',
+    md: '12px',
+    lg: '18px',
+    xl: '24px',
+    '2xl': '30px',
+    '3xl': '36px'
+}
---
+++ src/utils/spacing.jsx
@@ -0,0 +1,11 @@
+export const spacing = {
+    '3xs': '2px',
+    '2xs': '4px',
+    xs: '8px',
+    sm: '12px',
+    md: '16px',
+    lg: '24px',
+    xl: '32px',
+    '2xl': '40px',
+    '3xl': '56px'
+}
