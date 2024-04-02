918a462dbb49190fbd508f2311a5d0760a17e19e
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2024-04-02 11:01:01 +0100

    changed footer layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,52 +1,40 @@
 import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container,} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Footer() {
     return (
         <>
-            <div>
-                <Container style={{textAlign: 'center', fontFamily: '"Poppins", sans-serif'}} fluid>
-                    <div>
+            <Container style={{fontFamily: '"Poppins", sans-serif'}} fluid>
+                <hr />
+                <div className="row row-cols-2">
+                    <div className="col-7 d-flex flex-column  justify-content-start align-items-start">
                         <p style={{fontWeight: 200}}>(xxx)xxx-xxxx // info@example.com</p>
+                        <Link to={'/our story'} className="text-decoration-none text-primary">
+                            OUR STORY
+                        </Link>
+                        <Link to={'/contact'} className="text-decoration-none text-primary">
+                            FIND US
+                        </Link>
                     </div>
-                    <Container fluid>
-                        <Row>
-                            <Col md={{span: 1, offset: 5}}>
-                                <Link to={''} target="_blank">
-                                    <FontAwesomeIcon icon={faFacebookF} size="xl" className=""/>
-                                </Link>
-                            </Col>
-                            <Col md={{span: 1, offset: 0}}>
-                                <Link to={''} target="_blank">
-                                    <FontAwesomeIcon icon={faInstagram}size="xl" className=""/>
-                                </Link>
-                            </Col>
-                        </Row>
-                    </Container>
-                    <Container fluid>
-                        <Row style={{fontSize: 12, fontWeight: 600}}>
-                            <Col md={{span: 2, offset: 4}}>
-                                <Link to={'/our story'} className="text-decoration-none">
-                                    OUR STORY
-                                </Link>
-                            </Col>
-                            <Col md={{span: 2, offset: 0}}>
-                                <Link to={'/contact'} className="text-decoration-none">
-                                    FIND US
-                                </Link>
-                            </Col>
-                        </Row>
-                    </Container>
-                    <div>
+                    <div className="col-5 d-flex flex-column justify-content-end align-items-end">
+                        <p style={{fontWeight: 200}}>Follow Us</p>
+                        <Link to={''} target="_blank" className="mb-3">
+                            <FontAwesomeIcon icon={faFacebookF} size="xl" color={"#000"} />
+                        </Link>
+                        <Link to={''} target="_blank">
+                            <FontAwesomeIcon icon={faInstagram}size="xl" color={"red"} />
+                        </Link>
+                    </div>
+                    <div className="col-12 text-center">
                         <p>
-                        Developed by     
-                            <a href="" target="_blank" rel="noopener noreferrer">NetChunin</a>
-                        </p>
+                            <span>Developed by</span>
+                            <a href="" target="_blank" className="ms-1">NetChunin</a>
+                        </p> 
                     </div>
-                </Container>
-            </div>
+                </div>
+            </Container>
         </>
     )
 }
